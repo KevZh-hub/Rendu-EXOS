565f9f0abc3cfca31b16375e38d3f97052824b39
KevZh-hub <55091168+KevZh-hub@users.noreply.github.com>
2019-12-17 19:13:29 +0100

    Initial commit

---
+++ src/Exo 2/Chat.kt
@@ -0,0 +1,9 @@
+class Chat:Animal {
+    override fun parler() {
+        println("Meow")
+    }
+
+    override fun quiSuisJe() {
+        println("Je suis un chat")
+    }
+}
---
+++ src/Exo 2/Chien.kt
@@ -0,0 +1,10 @@
+class Chien:Animal {
+    override fun parler() {
+        println("Wouaf wof")
+    }
+
+    override fun quiSuisJe() {
+        println("Je suis un chien")
+    }
+
+}
